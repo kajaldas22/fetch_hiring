529d1df5c4a73dfafcca98ca6839a84dde90320e
Kajal Das <55104074+kajaldas22@users.noreply.github.com>
2024-02-19 17:51:50 -0800

    Update top5Brands_comp_curr_vs_prev.sql

--- top5Brands_comp_curr_vs_prev.sql
+++ top5Brands_comp_curr_vs_prev.sql
@@ -1,6 +1,7 @@
 --question 1:  What are the top 5 brands by receipts scanned for most recent month?
 -- due to current date data is not available, i have no choice to look for available dates. 
 
+-- previous vs current month by rank comparsion.
 with previousMonthRnk as (
 select 
     b.name as brand_name
@@ -12,14 +13,14 @@ join    mytestschema.rewardsreceiptitemlist r1
   on    f._id=r1.receipt_id
 join    mytestschema.brands b
   on    r1.barcode=b.barcode
-WHERE date_trunc('month',f.datescanned)<='2020-11-01'
+WHERE date_trunc('month',f.datescanned) between'2020-10-01' and '2020-11-01'
 --DATEDIFF(month, f.datescanned, CURRENT_DATE) <= 1
 group by 1
 order by 2 desc
 limit 5
 )
 
--- vs  current month data- 
+-- vs  current month
 , currentMonthRnk as (
  select 
     b.name as brand_name
@@ -31,10 +32,24 @@ join    mytestschema.rewardsreceiptitemlist r1
   on    f._id=r1.receipt_id
 join    mytestschema.brands b
   on    r1.barcode=b.barcode
-WHERE date_trunc('month',f.datescanned)>='2021-02-01'
+WHERE date_trunc('month',f.datescanned)>='2021-01-01'
 -- DATE_TRUNC('MONTH', r.datescanned) = DATE_TRUNC('MONTH', DATEADD('MONTH', -1, CURRENT_DATE))
     
 group by 1
 order by 2 desc
 limit 5
 )
+
+
+SELECT 'Current Month' AS month,
+       brand_name,
+       cnt,
+       current_mth_rnk
+FROM currentMonthRnk
+UNION ALL
+SELECT 'Previous Month' AS month,
+       brand_name,
+       cnt,
+       previous_mth_rnk
+FROM previousMonthRnk;
+ORDER BY month, current_mth_rnk;
